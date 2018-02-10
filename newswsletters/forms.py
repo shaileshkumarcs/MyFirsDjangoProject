9a5ef129ba4a1195001fd25fac08e7ea50f5d6d6
shailesh <shailesh.kumarcs@gmail.com>
2018-02-10 19:18:51 +0530

    Chapter 13 completed

--- newswsletters/forms.py
+++ newswsletters/forms.py
@@ -7,3 +7,18 @@ class SignUpForm(forms.ModelForm):
 		model = SignUp
 		fields = ['full_name','email']
 		####exclude = ['full_name']
+
+	def clean_email(self):
+		email =  self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split(".")
+		if not domain == "USE":
+			raise forms.ValidationError("Please make sure you use your USE email")
+		if not extension == "edu":
+			raise forms.ValidationError("Please use a valid .EDU email address")
+		return email
+
+	def clean_full_name(self):
+		full_name = self.cleaned_data.get('full_name')
+		return full_name
+
--- newswsletters/views.py
+++ newswsletters/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render(request,"home.html",{})
+	title = "Welcome"
+	if request.user.is_authenticated():
+		title = "My Title %s" %(request.user)
+
+	#add a form here	
+	contaxt = {
+		"title" : title,
+		"abc" : 123
+	}
+	return render(request,"home.html",contaxt)
