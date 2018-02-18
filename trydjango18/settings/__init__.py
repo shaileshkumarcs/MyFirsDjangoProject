c323207af7a42c325905b3fb25e45727943c1366
shailesh <shailesh.kumarcs@gmail.com>
2018-02-18 22:49:39 +0530

    Changes on setting try Django1.8

---
+++ trydjango18/settings/__init__.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+try:
+    from .local import *
+except:
+    pass
+
+try:
+    from .production import *
+except:
+    pass
+
+try:
+    from .imac import *
+except:
+    pass
+
+try:
+    from .macbookpro import *
+except:
+    pass
