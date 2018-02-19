fe0886296d4efd3b323272372f3c8fc1cdaa98ff
Shailesh Kumar Dangi <shailesh.kumarcs@gmail.com>
2018-02-19 10:17:35 +0530

    Update base.py

--- trydjango18/settings/base.py
+++ trydjango18/settings/base.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shailesh94kumar@gmail.com'
-EMAIL_HOST_PASSWORD = '9939744337'
+EMAIL_HOST_PASSWORD = 'xxxxxx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
